b7e4093211420c45a69d21220c0caa4918f3e196
wngrahams <wgs11@georgetown.edu>
2017-10-01 18:18:56 -0400

    Added ShapesList members

--- src/paint/ShapesList.java
+++ src/paint/ShapesList.java
@@ -1,9 +1,38 @@
 package paint;
 
+import java.util.ArrayList;
+
 public class ShapesList {
 
-	public ShapesList() {
-		// TODO Auto-generated constructor stub
+	private ArrayList<Shape> listOfShapes;
+	
+	private double totalPerimeter;
+	private double totalArea;
+
+	public double getTotalPerimeter() {
+		if (null != listOfShapes) { 
+			totalPerimeter = 0;
+			
+			for (int i=0; i<listOfShapes.size(); i++)
+				totalPerimeter += listOfShapes.get(i).calculatePerimeter();
+			
+			return totalPerimeter;
+		}
+		
+		return 0;
+	}
+	
+	public double getTotalArea() {
+		if (null != listOfShapes) {
+			totalArea = 0;
+			
+			for (int i=0; i<listOfShapes.size(); i++)
+				totalArea += listOfShapes.get(i).calculateArea();
+			
+			return totalArea;
+		}
+		
+		return 0;
 	}
 
 }
