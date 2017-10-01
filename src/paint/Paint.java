24643efa9d8f57cc5e0a3ea19770d0e8aefb0e7a
wngrahams <wgs11@georgetown.edu>
2017-10-01 14:42:10 -0400

    Got rid of unneeded constructor

--- src/paint/Paint.java
+++ src/paint/Paint.java
@@ -2,13 +2,8 @@ package paint;
 
 public class Paint {
 
-	public Paint() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("paint");
 
 	}
 
