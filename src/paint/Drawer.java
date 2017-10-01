3ef05a4a8507c22bf76d944f92b07082cae90f3a
wngrahams <wgs11@georgetown.edu>
2017-10-01 19:14:52 -0400

    Added basic GUI layout

--- src/paint/Drawer.java
+++ src/paint/Drawer.java
@@ -1,9 +1,81 @@
 package paint;
 
-public class Drawer {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+public class Drawer extends JFrame implements ActionListener {
+	
+	private JPanel buttonPanel;
+	private JPanel drawingPanel;
+	
+	private JButton areaButton;
+	private JButton lineButton;
+	private JButton ovalButton;
+	private JButton perimeterButton;
+	private JButton rectangleButton;
+	private JButton triangleButton;
 
 	public Drawer() {
-		// TODO Auto-generated constructor stub
+		initializePanels();
+		initializeButtons();
+		
+		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void initializeButtons() {
+		areaButton = new JButton("Get Area");
+		perimeterButton = new JButton("Get Perimeter");
+		
+		lineButton = new JButton(new ImageIcon("res/line.png"));
+		ovalButton = new JButton(new ImageIcon("res/oval.png"));
+		rectangleButton = new JButton(new ImageIcon("res/rectangle.png"));
+		triangleButton = new JButton(new ImageIcon("res/triangle.png"));
+		
+		buttonPanel.add(rectangleButton);
+		buttonPanel.add(ovalButton);
+		buttonPanel.add(triangleButton);
+		buttonPanel.add(lineButton);
+		buttonPanel.add(areaButton);
+		buttonPanel.add(perimeterButton);
+	}
+	
+	private void initializePanels() {
+		setTitle("Paint");
+		setSize(1200, 750);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	    setLayout(new BorderLayout());
+	    
+	    //Button panel
+	    buttonPanel = new JPanel();
+	    buttonPanel.setBackground(Color.LIGHT_GRAY);
+	    buttonPanel.setLayout(new GridLayout(1, 0));
+	    buttonPanel.setBorder(new LineBorder(Color.DARK_GRAY));
+	    
+	    
+	    add(buttonPanel, BorderLayout.SOUTH);
+	    
+	    
+	    //Drawing panel
+	    drawingPanel = new JPanel();
+	    drawingPanel.setBackground(Color.WHITE);
+	    
+	    add(drawingPanel, BorderLayout.CENTER);
 	}
 
 }
--- src/paint/Paint.java
+++ src/paint/Paint.java
@@ -3,8 +3,7 @@ package paint;
 public class Paint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		Drawer painter = new Drawer();
 	}
 
 }
