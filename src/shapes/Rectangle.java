c69b36d29892c90566de4dd5c97fd1abcaa30373
wngrahams <wgs11@georgetown.edu>
2017-10-05 16:19:15 -0400

    Removed unneeded exceptions

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -5,7 +5,7 @@ public class Rectangle extends Shape {
 	private double height;
 	private double width;
 
-	public Rectangle(double h, double w) throws Exception {
+	public Rectangle(double h, double w) {
 		height = h;
 		width = w;
 	}
--- src/shapes/Triangle.java
+++ src/shapes/Triangle.java
@@ -6,7 +6,7 @@ public class Triangle extends Shape {
 	private double side2;
 	private double side3;
 
-	public Triangle(double s1, double s2, double s3) throws Exception {
+	public Triangle(double s1, double s2, double s3) {
 		side1 = s1;
 		side2 = s2;
 		side3 = s3;
