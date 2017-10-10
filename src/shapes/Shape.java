6b3fc51643bbd9aa07deaebc481b1f146a8c7426
wngrahams <wgs11@georgetown.edu>
2017-10-09 21:56:21 -0400

    Added final static shape types

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -12,4 +12,9 @@ public abstract class Shape {
 	public abstract void drawShape(Graphics g, int[] pos);
 	public abstract void setDim(int[] newDim);
 
+	public final static Shape LINE = new Line();
+	public final static Shape OVAL = new Oval();
+	public final static Shape RECTANGLE = new Rectangle();
+	public final static Shape TRIANGLE = new Triangle();
 }
+
