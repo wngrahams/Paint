f843b521597404d0faeaa8a38cf23f06724f8117
wngrahams <wgs11@georgetown.edu>
2017-10-15 23:50:53 -0400

    Adjusting rectangles works for all corners and middle

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,6 +1,7 @@
 package shapes;
 
 import java.awt.Graphics;
+import java.util.Arrays;
 
 public class Rectangle extends Shape {
 	
@@ -21,6 +22,40 @@ public class Rectangle extends Shape {
 		location = new int[2];
 		this.setDim(newDim);
 	}
+	
+	@Override
+	public void adjust(int newX, int newY, int direction) {
+		int[] oldLoc = Arrays.copyOf(location, location.length);
+		
+		if (LOCATION_SE == direction)
+			setDim(newX - location[0], newY - location[1]);
+		else if (LOCATION_NW == direction) {
+			setLoc(newX, newY);
+			setDim((dimensions[0] + (oldLoc[0] - location[0])), (dimensions[1] + (oldLoc[1] - location[1])));
+		}
+		else if (LOCATION_SW == direction) {
+			setLoc(newX, location[1]);
+			setDim((dimensions[0] + (oldLoc[0] - location[0])), (newY - location[1]));
+		}
+		else if (LOCATION_NE == direction) {
+			setLoc(location[0], newY);
+			setDim((newX - location[0]), (dimensions[1] + (oldLoc[1] - location[1])));
+		}
+		else if (LOCATION_N == direction) {
+			setLoc(location[0], newY);
+			setDim(dimensions[0], (dimensions[1] + (oldLoc[1] - location[1])));
+		}
+		else if (LOCATION_W == direction) {
+			setLoc(newX, location[1]);
+			setDim((dimensions[0] + (oldLoc[0] - location[0])), dimensions[1]);
+		}
+		else if (LOCATION_S == direction)
+			setDim(dimensions[0], (newY - location[1]));
+		else if (LOCATION_E == direction)
+			setDim((newX - location[0]), dimensions[1]);
+		else if (LOCATION_MIDDLE == direction)
+			setLoc(newX, newY);
+	}
 
 	@Override
 	public double calculateArea() {
@@ -34,21 +69,35 @@ public class Rectangle extends Shape {
 	
 	@Override
 	public int contains(int x, int y) {
+		resetLocAndDim();
+		
 		if ((x >= location[0]) && (x <= location[0] + dimensions[0]) 
 				&& (y >= location[1]) && (y <= location[1] + dimensions[1])) {
 			
 			// Mouse is in the shape. Now determine what part of the shape:
-			if(x < location[0] + (dimensions[0]/2)) {
-				if (y < location[1] + (dimensions[1]/2)) 
+			if(x <= location[0] + (dimensions[0]/3)) {
+				if (y <= location[1] + (dimensions[1]/3)) 
 					return LOCATION_NW;
-				else 
+				else if (y >= location[1] + (dimensions[1] * 2/3))
 					return LOCATION_SW;
+				else
+					return LOCATION_W;
+			}
+			else if (x >= location[0] + (dimensions[0] * 2/3)) {
+				if (y <= location[1] + (dimensions[1]/3)) 
+					return LOCATION_NE;
+				else if (y >= location[1] + (dimensions[1] * 2/3))
+					return LOCATION_SE;
+				else
+					return LOCATION_E;
 			}
 			else {
-				if (y < location[1] + (dimensions[1]/2))
-					return LOCATION_NE;
-				else 
-					return LOCATION_SE;
+				if (y <= location[1] + (dimensions[1]/3)) 
+					return LOCATION_N;
+				else if (y >= location[1] + (dimensions[1] * 2/3))
+					return LOCATION_S;
+				else
+					return LOCATION_MIDDLE;
 			}
 		}
 		
@@ -76,6 +125,15 @@ public class Rectangle extends Shape {
 		g.fillRect(drawStart[0], drawStart[1], drawDim[0], drawDim[1]);
 	}
 	
+	private void resetLocAndDim() {
+		for (int i=0; i<dimensions.length; i++) {
+			if (dimensions[i] < 0) {
+				location[i] = location[i] + dimensions[i];
+				dimensions[i] = -1 * dimensions[i];
+			}
+		}
+	}
+	
 	@Override
 	public void setDim(int[] newDim) {
 		dimensions[0] = newDim[0];
@@ -91,5 +149,10 @@ public class Rectangle extends Shape {
 	public void setLoc(int[] newLoc) {
 		location[0] = newLoc[0];
 		location[1] = newLoc[1];
+	}
+	
+	public void setLoc(int x, int y) {
+		location[0] = x;
+		location[1] = y;
 	}
 }
